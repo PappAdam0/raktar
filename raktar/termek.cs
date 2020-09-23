0fde6ff89b63a76beb0b7781d61c49e227f86aeb
PappAdam0 <papp.adaam@gmail.com>
2020-09-23 14:37:50 +0200

    Add project files.

---
+++ raktar/termek.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace raktar
+{
+    class termek
+    {
+        private string kod;
+
+        public string KOD
+        {
+            get { return kod; }
+            set { kod = value; }
+        }
+
+    }
+}
